7e7c63f8b891251d0ae6d14d6ad5fef5c3a801d3
Prosanto Deb <60332621+Prosanto7@users.noreply.github.com>
2020-05-21 10:48:55 +0600

    Added Some Icons

--- Prosanto/src/Templates/Frame_Setup.java
+++ Prosanto/src/Templates/Frame_Setup.java
@@ -2,6 +2,7 @@
 package Templates;
 
 
+import java.awt.Color;
 import java.awt.Container;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,11 +12,23 @@ public class Frame_Setup extends JFrame
 {
     Container container;
    
-    public ImageIcon display_picture = new ImageIcon(this.getClass().getResource("/Pictures/display_picture.png"));
-    public ImageIcon login_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/login_button_icon.png"));
-
     
-    Frame_Setup()
+    public ImageIcon  display_picture = new ImageIcon(this.getClass().getResource("/Pictures/display_picture.png"));
+    public ImageIcon  login_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/login_button_icon.png"));
+    public ImageIcon  shop_name_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/shop_name_button_icon.png"));
+    public ImageIcon  create_sales_invoice_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/create_sales_invoice_button_icon.png"));
+    public ImageIcon  create_purchase_invoice_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/create_purchase_invoice_button_icon.png"));
+    public ImageIcon  logout_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/logout_button_icon.png"));  
+    public ImageIcon  home_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/home_button_icon.png"));
+    public ImageIcon  add_cost_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/add_cost_button_icon.png"));
+    public ImageIcon  daily_income_cost_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/daily_income_cost_button_icon.png"));
+    public ImageIcon  stock_check_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/stock_check_button_icon.png"));
+    public ImageIcon  due_check_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/due_check_button_icon.png"));
+    public ImageIcon  staff_attendence_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/staff_attendence_button_icon.png"));
+    public ImageIcon  settings_button_icon = new ImageIcon(this.getClass().getResource("/Pictures/settings_button_icon.png"));
+    
+    
+    public Frame_Setup()
     {
         setFrame();
         setContainer();
@@ -33,6 +46,7 @@ public class Frame_Setup extends JFrame
     public void setContainer()
     {
         container = this.getContentPane();
+        container.setBackground(Color.WHITE);
     }
     
     
